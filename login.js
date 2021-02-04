39eb00585eb822bfd46806fd80955ee0b5a0580c
Yash Savalia <67118117+yashsavalia1@users.noreply.github.com>
2021-02-04 02:04:31 -0500

    Update login.ls

--- login.js
+++ login.js
@@ -2,12 +2,12 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 //To be replaced with strings of ID Numbers
-const idnums = ["1","2","3","4","5","6","7","8","9",
-                "11","12","13","14","15","16","17","18","19",
-                "21","22","23","24","25","26","27","28","29",
-                "31","32","33","34","35","36","37","38","39",
-                "41","42","43","44","45","46","47","48","49",
-                "51","52","53","54","55","56","57","58","59"]
+const idnums = ["1","2","3","4","5","6","7","8","9", "!0",
+                "11","12","13","14","15","16","17","18","19", "20",
+                "21","22","23","24","25","26","27","28","29", "30",
+                "31","32","33","34","35","36","37","38","39", "40",
+                "41","42","43","44","45","46","47","48","49", "50",
+                "51","52","53","54","55","56","57","58","59", "60"]
 
 //Checks whether idnums contains the inputed string
 loginButton.addEventListener("click", (e) => {
